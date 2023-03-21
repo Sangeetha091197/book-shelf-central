364cc43b89447f1b1c98819cfae7770b4ff5b31e
Sangeetha Shankar <sangeethashankar091197@gmail.com>
2023-03-21 17:00:45 +0530

    configured routing module

--- book-shelf-central/src/app/app-routing.module.ts
+++ book-shelf-central/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthorListComponent } from './author-list/author-list.component';
+import { BookListComponent } from './book-list/book-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AuthorListComponent
+  },
+  {
+    path: 'book-list', component: BookListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
